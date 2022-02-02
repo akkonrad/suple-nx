5b775828f1a77bae2c62d0d268dccd5135c27fe9
Konrad <konrad@akkroweb.com>
2022-02-02 17:11:17 +0100

    adding graphql

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'suple-nx-nest-neo-gql-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
