01f2ac0cf0e55deaa9e0f0ec765774d699cd907e
Konrad <konrad@akkroweb.com>
2022-02-22 00:02:04 +0100

    update codebase; toolbar added

--- apps/frontend/src/app/core/toolbar/toolbar.component.ts
+++ apps/frontend/src/app/core/toolbar/toolbar.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'front-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+      icon: PrimeIcons.HOME,
+      routerLink: ['']
+    },
+    {
+      label: 'Supplements',
+      icon: PrimeIcons.HEART,
+      items: [
+        {
+          label: 'Add',
+          icon: PrimeIcons.PLUS,
+          routerLink: ['suple', 'add']
+        },
+        {
+          label: 'List',
+          icon: PrimeIcons.LIST,
+          routerLink: ['suple', 'list']
+        }
+      ]
+    },
+    {
+      label: 'Health results',
+      icon: PrimeIcons.CHART_BAR,
+      items: [
+        {
+          label: 'Add',
+          icon: PrimeIcons.PLUS,
+          routerLink: ['suple', 'add']
+        },
+        {
+          label: 'List',
+          icon: PrimeIcons.LIST,
+          routerLink: ['suple', 'list']
+        }
+      ]
+    },
+    {
+      label: 'Admin',
+      icon: PrimeIcons.LOCK_OPEN,
+      styleClass: 'p-ml-6',
+      items: [
+        {
+          label: 'Medical Publications',
+          icon: PrimeIcons.PLUS_CIRCLE
+        },
+        {
+          label: 'Items',
+          icon: PrimeIcons.LIST
+        },
+        {
+          label: 'Settings',
+          icon: PrimeIcons.COG
+        }
+      ]
+    }
+  ];
 
 }
