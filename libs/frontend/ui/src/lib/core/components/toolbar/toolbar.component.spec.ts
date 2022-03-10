285e0642b89444405bf965ccbbaa4830a52b211f
Konrad <konrad@akkroweb.com>
2022-03-11 00:22:54 +0100

    fix toolbar tests

--- libs/frontend/ui/src/lib/core/components/toolbar/toolbar.component.spec.ts
+++ libs/frontend/ui/src/lib/core/components/toolbar/toolbar.component.spec.ts
@@ -2,17 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarComponent } from './toolbar.component';
 import { ThemeModule } from '@suple/frontend/ui';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       declarations: [ToolbarComponent],
-      imports: [ThemeModule]
+      imports: [ThemeModule, RouterTestingModule]
     })
       .compileComponents();
+
   });
 
   beforeEach(() => {
