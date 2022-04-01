96949bde794b28844f4275bb7abc019ce1389055
Konrad <konrad@akkroweb.com>
2022-04-01 22:39:52 +0200

    create suple form * adding suple form * adding ingredients component * handling some logic * ux improvement

--- README.md
+++ README.md
@@ -1,5 +1,77 @@
 # Suple
 
-This project was generated using [Nx](https://nx.dev).
+This is an example file with default selections.
 
-Docs: TODO
+## Install
+
+Install all dependencies.
+
+Prequisities: you need to have neo4j database at some point (TODO)
+
+```
+yarn install
+```
+
+## Run app
+
+Run each job separately
+
+```
+yarn go:be // backend
+yarn go:fe // frontend
+yarn go:db // neo4j database - TODO
+```
+
+## Development
+
+```
+
+```
+
+### Project structure
+
+frontend - backend - libraries -
+
+- ui
+- data
+- b
+- c
+
+```
+TODO
+```
+
+### Create new library
+
+```
+nx g @nrwl/workspace:library <path/library name> --dryRun
+nx g @nrwl/angular:library <app-name/library type> --dryRun
+```
+
+### Create a new module in library
+
+```
+nx g @nrwl/angular:module <module name> --project=<library name> --dryRun
+```
+
+### Create new component in a library
+
+```
+nx g @nrwl/angular:component <path/component-name> --project=<library name> --dryRun
+```
+
+## Tests
+
+TODO
+
+## Deployment
+
+TODO
+
+## Contributing
+
+PRs accepted.
+
+## License
+
+MIT © Richard McRichface.
--- libs/frontend/ui/src/lib/suple/suple-create-page/types/suple-form-group.ts
+++ libs/frontend/ui/src/lib/suple/suple-create-page/types/suple-form-group.ts
@@ -1,4 +1,4 @@
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Ingredient } from '@suple/shared/types';
 
 interface SupleFormData {
@@ -12,7 +12,15 @@ export class SupleFormGroup extends FormGroup {
     super({
       name: new FormControl(supleFormData?.name, Validators.required),
       description: new FormControl(supleFormData?.description, Validators.required),
-      ingredients: new FormArray(supleFormData?.ingredients.map(ingredient => new FormControl(ingredient)) || [])
+      ingredients: new FormArray(supleFormData?.ingredients.map(ingredient => new FormControl(ingredient)) || [], validateFieldSumEqual())
     });
   }
 }
+
+const validateFieldSumEqual = (): ValidationErrors => {
+  return (formArray: FormArray): null | { error: string } => {
+    let sum: number = 0;
+    formArray.controls.forEach((control: AbstractControl) => sum += control.value.quantity);
+    return sum <= 100 ? null : { error: 'Overall quantity needs to be less than 100' };
+  };
+};
